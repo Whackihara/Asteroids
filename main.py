1aade23b29b6dee6c2dd43e6c81bea91788152e3
Whackihara <Whackihara@proton.me>
2024-12-29 06:55:41 -0500

    added circleshape.py

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    clock = pygame.time.Clock()
+    dt = 0
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(COLORS["BLK"])
         pygame.display.flip()
-
+        # tick for 1/60th of a second, convert delta to seconds (ms->s)
+        dt = (clock.tick(60)/1000)
+        
 if __name__ == "__main__":
     main()
 
